d6a756ace42f7ef4327a66187dcebec978da2ed9
MarcinK420 <marcin.kruk@uth.pl>
2025-03-06 17:40:44 +0100

    ver 0.1
    ekran powitalny (nieprzetestowane)

---
+++ Fiszkoteka/app/src/main/java/com/example/fiszkoteka/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.fiszkoteka
+
+class MainActivity {
+
+}
---
+++ Fiszkoteka/app/src/main/java/com/example/fiszkoteka/WelcomeActivity.kt
@@ -0,0 +1,21 @@
+package com.example.fiszkoteka
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+
+class WelcomeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_welcome)
+
+        val getStartedButton = findViewById<Button>(R.id.getStartedButton)
+        getStartedButton.setOnClickListener {
+            // Przejście do głównej aktywności
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish() // Zakończenie bieżącej aktywności
+        }
+    }
+}
